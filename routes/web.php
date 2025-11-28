b15f9de300b0cc96b5911605bd9e18e6c60b24bc
fatmagndgd03 <fatma.gundogdu@gazi.edu.tr>
2025-11-28 11:05:39 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hakkimizda', function () {
+    return "<h1>Şirketimiz Hakkında Bilgiler...</h1>";
+});
+  
